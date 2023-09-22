aa6779d3226b5ee9fe6f2eb91b8b6d60715c11f5
Shivanigupta3003 <145620538+Shivanigupta3003@users.noreply.github.com>
2023-09-22 18:49:17 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code as newer version//
+
+// alert(3+3) // we are using node js, not browser
+
+console.log(3
+    +
+    3) // code redability should be high //
+
+    console.log("Shivani");
+
+    let name = "Shivani"
+    let age = 23
+    let isLoggedIn = false
+    let state;
+
+    // number = 2 to power 53 //
+    // bigint //
+    // string = "" //
+    // boolean = true/false //
+    // null = standalone value //
+    // symbol = unique //
+
+
+    // object //
+
+    console.log(typeof undefined);
+    console.log(typeof null);
