7437f1c843b224161e513ec374d13219102e76ca
Shivanigupta3003 <145620538+Shivanigupta3003@users.noreply.github.com>
2023-09-22 21:09:04 +0000

    conversion of datatypes and operations

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,66 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// 25 = 25 //
+// "shivani" = NaN //
+// "25" = string //
+// "25abcd" = NaN //
+// true = 1, false = 0 //
+
+let isLoggedIn = "Shivani"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// "Shivani" = True //
+// "" = False //
+// true = 1, false = 0 //
+
+let someNumber = 90
+
+let stringNumber = String(someNumber)
+
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//****************operations**********
+
+let value = 6
+let negvalue = -value
+
+//console.log(negvalue);
+
+let str1 = "hi"
+let str2 = "shivani"
+
+let str3 = str1 + str2
+
+//console.log(str3);
+
+//console.log(3 + 2);
+//console.log("3" + 1);
+//console.log(3 + "2");
+//console.log("3" + 2 + 7);
+//console.log(3 + 2 + "7");
+
+//console.log((3 + 5) * 7 % 9);
+
+//console.log(+true);
+//console.log(true+);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 9 + 4
+
+let gameCounter = 303
+++gameCounter;
+
+console.log(gameCounter);
